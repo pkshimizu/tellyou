736c42bc7afe16754bf5f9cdf4f199abefecac8c
shimizu.kenji <shimizu.kenji@noncore.net>
2025-10-25 16:12:48 +0900

    create project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:tellyou/ui/home/screen.dart';
+
+void main() {
+  runApp(const TellYouApp());
+}
+
+class TellYouApp extends StatelessWidget {
+  const TellYouApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TellYou',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomeScreen(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
